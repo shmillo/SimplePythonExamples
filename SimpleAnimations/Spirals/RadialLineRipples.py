bd3d6b672d9778735ea54669b2300e04b45a84f8
Shawn Milloway <millowayengineering@gmail.com>
2023-01-25 13:00:03 -0500

    Add files via upload

---
+++ SimpleAnimations/Spirals/RadialLineRipples.py
@@ -0,0 +1,49 @@
+import numpy as np
+import matplotlib.pyplot as mplt
+from matplotlib.animation import FuncAnimation
+
+def exponentialEnvelope(x):
+    return np.exp(-240.0 * x )
+
+fig, ax = mplt.subplots(1); fig.tight_layout(pad=0); fig.set_facecolor('black')
+ax.axis('off'); ax.set_facecolor('black')
+
+leftPoint = 0.0; rightPoint = 2.0
+spatialResolution = 1000; l = int((rightPoint - leftPoint) * spatialResolution); w = int((rightPoint - leftPoint) * spatialResolution)
+hashGrid = np.zeros([l + 1, w + 1])
+
+numLines = 21; print("nLines", numLines); radians = np.linspace(0, 360, num=numLines)
+x = []; x = np.array(x)
+
+lineThickness = 1000
+for r in range(1, radians.shape[0], 2):
+    startPoint = radians[r-1]; endPoint = radians[r]; spacing = (endPoint - startPoint)/lineThickness
+    x = np.concatenate( (x, np.linspace(startPoint,  endPoint, num=lineThickness)) )
+x *= np.pi/180.0; y = np.copy(x); angleX = np.cos(x); angleY = np.sin(y)
+
+lineThickness = 1000
+for r in range(1, radians.shape[0], 2):
+  startPoint = radians[r-1]; endPoint = radians[r]; spacing = (endPoint - startPoint)/lineThickness
+  x = np.concatenate( (x, np.linspace(startPoint, endPoint, num=lineThickness)) )
+x *= np.pi/180.0; y = np.copy(x); angleX = np.cos(x); angleY = np.sin(y)
+
+angleOfRotation = 1.0; rotationRate = 1.0
+
+numSegments = 1000
+for segment in range(1, numSegments + 1):
+
+  angleOfRotation += rotationRate/segment
+  cosScalar = np.cos(angleOfRotation); sinScalar = np.sin(angleOfRotation)
+
+  currentRadius = segment/numSegments; radius = int(spatialResolution * currentRadius)
+  outputX = angleX*radius; outputY = angleY*radius
+
+  xTemp = (cosScalar*outputX) - (sinScalar*outputY); yTemp = (cosScalar*outputY) + (sinScalar*outputX)
+
+  x = np.array( np.round(xTemp), dtype=np.int32 ); y = np.array( np.round(yTemp), dtype=np.int32 )
+
+  hashGrid[x+spatialResolution, y+spatialResolution] = 1.0
+
+mplt.xlim([0.25*l, 0.75*l]); mplt.ylim([0.25*w, 0.75*w]); mplt.imshow(hashGrid, cmap='ocean')
+
+mplt.show()
