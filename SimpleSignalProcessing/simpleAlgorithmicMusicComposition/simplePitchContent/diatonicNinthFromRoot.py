52c58ab0c8ef095ebc6a6b9bdb5944f49b63289c
Shawn Milloway <millowayengineering@gmail.com>
2023-12-11 11:07:06 -0500

    Add files via upload

---
+++ SimpleSignalProcessing/simpleAlgorithmicMusicComposition/simplePitchContent/diatonicNinthFromRoot.py
@@ -0,0 +1,58 @@
+def indexesOfDiatonicTriadsInAMode(romanNumeralRootOfChord):
+    indexesToReturn = []
+    if(romanNumeralRootOfChord == 1):
+        indexesToReturn = list([0, 2, 4])
+    elif(romanNumeralRootOfChord == 2):
+        indexesToReturn = list([1, 3, 6])
+    elif(romanNumeralRootOfChord == 3):
+        indexesToReturn = list([2, 4, 7])
+    elif(romanNumeralRootOfChord == 4):
+        indexesToReturn = list([3, 6, 0])
+    elif(romanNumeralRootOfChord == 5):
+        indexesToReturn = list([4, 7, 1])
+    elif(romanNumeralRootOfChord == 6):
+        indexesToReturn = list([5, 0, 2])
+    elif(romanNumeralRootOfChord == 7):
+        indexesToReturn = list([6, 1, 3])
+
+    return indexesToReturn
+
+def diatonicSeventhChord(romanNumeralRootOfChord):
+    indexesToReturn = indexesOfDiatonicTriadsInAMode(romanNumeralRootOfChord)
+    if(romanNumeralRootOfChord == 1):
+        indexesToReturn.append(6)
+    elif(romanNumeralRootOfChord == 2):
+        indexesToReturn.append(0)
+    elif(romanNumeralRootOfChord == 3):
+        indexesToReturn.append(1)
+    elif(romanNumeralRootOfChord == 4):
+        indexesToReturn.append(2)
+    elif(romanNumeralRootOfChord == 5):
+        indexesToReturn.append(3)
+    elif(romanNumeralRootOfChord == 6):
+        indexesToReturn.append(4)
+    elif(romanNumeralRootOfChord == 7):
+        indexesToReturn.append(5)
+
+    return indexesToReturn   
+
+def diatonicNinthChord(romanNumeralRootOfChord):
+    indexesToReturn = diatonicSeventhChord(romanNumeralRootOfChord)
+    if(romanNumeralRootOfChord == 1):
+        indexesToReturn.append(1)
+    elif(romanNumeralRootOfChord == 2):
+        indexesToReturn.append(2)
+    elif(romanNumeralRootOfChord == 3):
+        indexesToReturn.append(3)
+    elif(romanNumeralRootOfChord == 4):
+        indexesToReturn.append(4)
+    elif(romanNumeralRootOfChord == 5):
+        indexesToReturn.append(5)
+    elif(romanNumeralRootOfChord == 6):
+        indexesToReturn.append(6)
+    elif(romanNumeralRootOfChord == 7):
+        indexesToReturn.append(0)
+
+    return indexesToReturn 
+
+print(diatonicNinthChord(1))
